47c8e47533f66666a892eba83ced38545ad14736
Pawel Srodek <pawel.srodek@outlook.com>
2026-06-01 19:24:20 +0200

    Dodanie Struktury Projektu

---
+++ src/test/java/Checkers_Tactics/MainTest.java
@@ -0,0 +1,12 @@
+package Checkers_Tactics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void exampleTest() {
+        assertTrue(true);
+    }
