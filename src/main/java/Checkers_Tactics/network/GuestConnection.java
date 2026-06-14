c6eccfbaf4c407f0398089babef893e65ecccc41
Pawel Srodek <pawel.srodek@outlook.com>
2026-06-14 14:22:29 +0200

    Edycja klas odpowiedzialnych za sockety, rozbudowanie zarzadzaniem polaczenia, dodanie oblugi bledow i zamykanie socketow

--- src/main/java/Checkers_Tactics/network/GuestConnection.java
+++ src/main/java/Checkers_Tactics/network/GuestConnection.java
@@ -3,12 +3,14 @@ package Checkers_Tactics.network;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class GuestConnection {
+public class GuestConnection implements NetworkConnection {
 
     private final String hostAddress;
     private final int port;
+    private final int connectTimeoutMillis;
 
     private Socket socket;
 
@@ -16,27 +18,43 @@ public class GuestConnection {
     private ObjectInputStream input;
 
     public GuestConnection(String hostAddress, int port) {
+        this(hostAddress, port, 10_000);
+    }
+
+    public GuestConnection(String hostAddress, int port, int connectTimeoutMillis) {
         this.hostAddress = hostAddress;
         this.port = port;
+        this.connectTimeoutMillis = connectTimeoutMillis;
     }
 
     public void connect() throws IOException {
-        socket = new Socket(hostAddress, port);
-
-        output = new ObjectOutputStream(socket.getOutputStream());
-
-        output.flush();
-
-        input = new ObjectInputStream(socket.getInputStream());
+        socket = new Socket();
+        try {
+            socket.connect(new InetSocketAddress(hostAddress, port), connectTimeoutMillis);
+            output = new ObjectOutputStream(socket.getOutputStream());
+            output.flush();
+            input = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException exception) {
+            close();
+            throw exception;
+        }
     }
 
-    public void send(NetworkMessage message) throws IOException {
+    @Override
+    public synchronized void send(NetworkMessage message) throws IOException {
+        if (output == null) {
+            throw new IOException("Brak polaczenia z hostem");
+        }
         output.writeObject(message);
         output.flush();
+        output.reset();
     }
 
+    @Override
     public NetworkMessage receive() throws IOException, ClassNotFoundException {
-
+        if (input == null) {
+            throw new IOException("Brak polaczenia z hostem");
+        }
         Object receivedObject = input.readObject();
 
         if (receivedObject instanceof NetworkMessage message) {
@@ -44,5 +62,27 @@ public class GuestConnection {
         }
 
         throw new IOException("Odebrano nieobslugiwany obiekt");
+    }
+
+    @Override
+    public boolean isConnected() {
+        return socket != null && socket.isConnected() && !socket.isClosed();
+    }
+
+    @Override
+    public void close() {
+        closeQuietly(input);
+        closeQuietly(output);
+        closeQuietly(socket);
+    }
+
+    private void closeQuietly(AutoCloseable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        } catch (Exception ignored) {
+        }
     }
 }
